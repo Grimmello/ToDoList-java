a7359645f40141f26358c80bfec4eb1e098e992a
Clifford Grimmell and chance magno <clifford@>
2017-03-21 10:26:50 -0700

    followed along and created ToDo list app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -11,9 +13,26 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", request.session().attribute("tasks"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<ToDo> tasks = request.session().attribute("tasks");
+      if (tasks == null) {
+        tasks = new ArrayList<ToDo>();
+        request.session().attribute("tasks", tasks);
+      }
+
+      String description = request.queryParams("description");
+      ToDo newTask = new ToDo(description);
+      tasks.add(newTask);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -2,5 +2,14 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ToDoTest {
-
+  @Test
+  public void ToDoItem_instantiatesCorrectly_true() {
+    ToDo myTask = new ToDo("Mow the lawn");
+    assertEquals(true, myTask instanceof ToDo);
+  }
+  @Test
+  public void ToDoItem_createsDescription_String() {
+    ToDo myTask = new ToDo("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
 }
